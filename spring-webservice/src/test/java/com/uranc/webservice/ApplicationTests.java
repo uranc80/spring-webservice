acfdecd2e0981081f5b338f10ba03af21e737aec
ADMIN\admin <uranc80@gmail.com>
2018-05-29 08:59:37 +0900

    2

--- spring-webservice/src/test/java/com/uranc/webservice/ApplicationTests.java
+++ spring-webservice/src/test/java/com/uranc/webservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.uranc.springwebservice;
+package com.uranc.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringWebserviceApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
