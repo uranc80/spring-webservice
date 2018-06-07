711a6a20dc4808985578a7479164b5b0b9f47aa5
ADMIN\admin <uranc80@gmail.com>
2018-06-07 12:08:15 +0900

    11

--- spring-webservice/src/main/java/com/uranc/webservice/service/PostsService.java
+++ spring-webservice/src/main/java/com/uranc/webservice/service/PostsService.java
@@ -1,10 +1,13 @@
 package com.uranc.webservice.service;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.uranc.webservice.domain.posts.PostsRepository;
+import com.uranc.webservice.dto.PostsMainResponseDto;
 import com.uranc.webservice.dto.posts.PostsSaveRequestDto;
 
 import lombok.AllArgsConstructor;
@@ -17,5 +20,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+    
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
